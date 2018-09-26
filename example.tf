686d86faa78d32097b1da91d0465ad70df0c9d5b
carlos ramirez <carlos.ramirez1@unmsm.edu.pe>
2018-09-26 11:55:00 -0500

    changes

--- example.tf
+++ example.tf
@@ -2,8 +2,8 @@ provider "telefonicaopencloud" {
     user_name   = "carlos.ramirezv@huawei.com"
     region      = "sa-peru-1"
     auth_url    = "https://auth.opencloud.telefonica.com.pe/authui/login.action"
-    access_key  = "7IPGS1JVMDPBFUEXLUYW"
-    secret_key  = "yFxvCLFzoDQSBehI8egMnIfrNiyvWwrdsB1qRdCf"
+    access_key  = "GKQNCNS2ZROA2RZCO78T"
+    secret_key  = "NMuMKnUbJmVe27e0GF62prtcgqbAKsIcKf040cHr"
     domain_name = "carlos.ramirezv"
     domain_id   = "0260ac05e0724a8ba7520466a920319e"
 }
